cfd445f6b55a3ea21a39222ab7e593c4d5dc8690
Ed Iwanski <eiwanski@containerstore.com>
2024-11-25 02:08:57 -0600

    chore: move hack-nf order

--- build_files/dx/04-override-install-dx.sh
+++ build_files/dx/04-override-install-dx.sh
@@ -14,14 +14,7 @@ unzip -qo /tmp/monaspace-font.zip -d /tmp/monaspace-font
 mkdir -p /usr/share/fonts/monaspace
 mv /tmp/monaspace-font/monaspace-v*/fonts/variable/* /usr/share/fonts/monaspace/
 rm -rf /tmp/monaspace-font*
-
 fc-cache -f /usr/share/fonts/monaspace
-fc-cache --system-only --really-force --verbose
-
-# globalproect-openconnect
-curl -s https://api.github.com/repos/yuezk/GlobalProtect-openconnect/releases/latest | jq -r '.assets[] | select(.name | contains ("x86_64.rpm")) | .browser_download_url' | head -n 1 | xargs -I {} wget -O /tmp/globalprotect-openconnect-latest.x86_64.rpm {}
-rpm-ostree install /tmp/globalprotect-openconnect-latest.x86_64.rpm
-rm -r -f /tmp/globalprotect-openconnect-latest.x86_64.rpm
 
 # Hack nerd-font
 curl -s https://api.github.com/repos/ryanoasis/nerd-fonts/releases/latest | jq -r '.assets[] | select(.name | contains ("Hack.zip")) | .browser_download_url' | head -n 1 | xargs -I {} wget -O /tmp/hack-nf-latest.zip {}
@@ -29,3 +22,11 @@ unzip -qo /tmp/hack-nf-latest.zip -d /tmp/hack-nf-fonts
 mkdir -p /usr/share/fonts/hack-nf-fonts
 mv /tmp/hack-nf-fonts/* /usr/share/fonts/hack-nf-fonts/
 rm -rf /tmp/hack-nf*
+fc-cache -f /usr/share/fonts/hack-nf-fonts
+
+fc-cache --system-only --really-force --verbose
+
+# globalproect-openconnect
+curl -s https://api.github.com/repos/yuezk/GlobalProtect-openconnect/releases/latest | jq -r '.assets[] | select(.name | contains ("x86_64.rpm")) | .browser_download_url' | head -n 1 | xargs -I {} wget -O /tmp/globalprotect-openconnect-latest.x86_64.rpm {}
+rpm-ostree install /tmp/globalprotect-openconnect-latest.x86_64.rpm
+rm -r -f /tmp/globalprotect-openconnect-latest.x86_64.rpm
