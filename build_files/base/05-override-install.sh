0d25e5e724ce133764f2c726264491a4a60a3830
Ed Iwanski <eiwanski@containerstore.com>
2025-11-20 08:45:45 -0600

    bug(dx): fix customization script for font and globaprotect

--- build_files/base/05-override-install.sh
+++ build_files/base/05-override-install.sh
@@ -57,7 +57,7 @@ echo "Compiling gschema to include bluefin setting overrides"
 glib-compile-schemas /usr/share/glib-2.0/schemas &>/dev/null
 
 # Hack nerd-font
-curl -s https://api.github.com/repos/ryanoasis/nerd-fonts/releases/latest | jq -r '.assets[] | select(.name | contains ("Hack.zip")) | .browser_download_url' | head -n 1 | xargs -I {} wget -O /tmp/hack-nf-latest.zip {}
+curl -s -o /tmp/hack-nf-latest.zip -L https://github.com/ryanoasis/nerd-fonts/releases/latest/download/Hack.zip
 unzip -qo /tmp/hack-nf-latest.zip -d /tmp/hack-nf-fonts
 mkdir -p /usr/share/fonts/hack-nf-fonts
 mv /tmp/hack-nf-fonts/* /usr/share/fonts/hack-nf-fonts/
@@ -66,7 +66,7 @@ fc-cache -f /usr/share/fonts/hack-nf-fonts
 fc-cache --system-only --really-force --verbose
 
 # globalproect-openconnect
-curl -s https://api.github.com/repos/yuezk/GlobalProtect-openconnect/releases/latest | jq -r '.assets[] | select(.name | contains ("x86_64.rpm")) | .browser_download_url' | head -n 1 | xargs -I {} wget -O /tmp/globalprotect-openconnect-latest.x86_64.rpm {}
+curl -s -o /tmp/globalprotect-openconnect-latest.x86_64.rpm -L $(curl -s https://api.github.com/repos/yuezk/GlobalProtect-openconnect/releases/latest | jq -r '.assets[] | select(.name | contains ("x86_64.rpm")) | .browser_download_url' | head -n 1)
 rpm-ostree install /tmp/globalprotect-openconnect-latest.x86_64.rpm
 rm -r -f /tmp/globalprotect-openconnect-latest.x86_64.rpm
 
